050356881d1d015c9ce6341d7f6a02eb2dfc8e70
Kota Ogino <okitsu090807@gmail.com>
2020-04-07 23:10:06 +0900

    edit

--- src/pages/index.js
+++ src/pages/index.js
@@ -35,7 +35,7 @@ const BlogIndex = ({ data, location }) => {
 
   return (
     <Layout location={location} title={siteTitle}>
-      <SEO title="All posts" />
+      <SEO title="ホーム" />
       {posts.map(({ node }) => {
         const title = node.frontmatter.title || node.fields.slug
         return (
