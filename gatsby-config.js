ebe9a8e3b8547ba6ac3e8b6081ca220f1105cedc
Kota Ogino <okitsu090807@gmail.com>
2020-04-10 23:51:47 +0900

    edit

--- gatsby-config.js
+++ gatsby-config.js
@@ -80,5 +80,14 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        // Setting a color is optional.
+        color: `#006e54`,
+        // Disable the loading spinner.
+        showSpinner: true,
+      },
+    },
   ],
 }
